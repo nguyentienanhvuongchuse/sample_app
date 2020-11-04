6b0c4c099e1cea9c26fed6ca1bc87355fcfd25f5
nguyentienanhvuongchuse <nguyenvuong.09062000@gmail.com>
2020-11-04 15:14:06 +0700

    Micropost

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
+require("packs/size_img")
 import "bootstrap"
 
 
@@ -17,3 +18,8 @@ import "bootstrap"
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+//= require i18n
+
+//= require i18n.js
+
+//= require i18n/translations
---
+++ app/models/micropost.rb
@@ -0,0 +1,18 @@
+class Micropost < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+  scope :newest, ->{order(created_at: :desc)}
+  validates :user_id, presence: true
+  validates :content, presence: true, length:
+    {maximum: Settings.micropost.max_value}
+  validates :image,
+            content_type: {in: %i(jpg gif png jpeg),
+                           message: I18n.t("micropost.img_valid")},
+            size: {less_than: Settings.micropost.mb.megabytes,
+                   message: I18n.t("micropost.less_than_5")}
+
+  def display_image
+    image.variant(resize_to_limit: [Settings.micropost.width,
+      Settings.micropost.height])
+  end
+end
